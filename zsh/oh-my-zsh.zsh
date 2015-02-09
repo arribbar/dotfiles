318232dd90e7f37d84f4d8bb203086083f694bd4
Arthur Puyou <arthur@puyou.fr>
2015-02-09 16:53:28 +0100

    Update zsh

--- zsh/oh-my-zsh.zsh
+++ zsh/oh-my-zsh.zsh
@@ -33,6 +33,6 @@ ZSH_THEME="clean2"
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
-plugins=(git)
+plugins=(git zshmarks)
 
 source $ZSH/oh-my-zsh.sh
