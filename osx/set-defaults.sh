30f8425de71ca28292a4626babbb907aef213610
Arthur Puyou <arthur@puyou.fr>
2016-07-25 22:26:33 +0200

    Remove Terminal auto-marks

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -36,3 +36,6 @@ defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
 defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+
+# Disable Terminal auto-mark
+defaults write com.apple.Terminal AutoMarkPromptLines -int 0
