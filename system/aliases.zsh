2dd69e17bd9644f737998cff8f5839813767eaf7
Arthur Puyou <arthur@puyou.fr>
2016-11-07 13:18:32 +0100

    gill alias

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -3,3 +3,4 @@ alias l="ls -lAh --color"
 alias ll="ls -l --color"
 alias la='ls -A --color'
 alias ql="qlmanage -p $? > /dev/null 2>&1"
+alias gill="docker-compose run --rm -u root web ./manage.py"
