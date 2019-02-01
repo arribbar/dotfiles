508aaec384024a1a0ec2d425719ba31433b5caab
Arthur Puyou <arthur@puyou.fr>
2019-02-01 15:51:04 +0100

    Update dotfiles

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -4,3 +4,6 @@ alias ql="qlmanage -p $? > /dev/null 2>&1"
 alias manage="docker-compose run --rm -u root web ./manage.py"
 alias wz="docker-compose run --rm www"
 alias phraseapp='docker run --rm -ti -v $(PWD):/code -w /code francois2/phraseapp'
+alias mirror='wget -mkEpnp'
+alias goelb='goaccess access.log --log-format='%^ %dT%t.%^ %v %h:%^ %^ %T %^ %^ %s %^ %b %^ "%r" "%u" %^' --date-format='%Y-%m-%d' --time-format=%T'
+alias s='subl .'
